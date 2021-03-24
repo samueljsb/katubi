c0fec55fba4f5a879e010ee34a8a47369ff8defe
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-03-24 21:47:47 +0000

    Use a Postgres database

--- src/katubi/settings.py
+++ src/katubi/settings.py
@@ -95,8 +95,12 @@ USE_TZ = True
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": SRC_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT", default="5432"),
     }
 }
 
