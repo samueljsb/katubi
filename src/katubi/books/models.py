9d48b07d1a110376b057a43c2e2be55612fc25f8
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-05-02 23:21:18 +0100

    Add a get_or_create method to Book model
    This allows us to look up a book by title and author(s) and create a
    record if one does not already exist.
    
    Also change the `new` method to take a sequence of Author objects,
    eather than handlign creation itself.

--- src/katubi/books/models.py
+++ src/katubi/books/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Sequence, Tuple
 
 from django.db import models
 
@@ -31,14 +31,46 @@ class Book(models.Model):
 
     @classmethod
     def new(
-        cls, *, title: str, subtitle: str, authors: List[str], description: str
+        cls, *, title: str, subtitle: str, authors: Sequence[Author], description: str
     ) -> Book:
         book = cls.objects.create(
             title=title, subtitle=subtitle, description=description
         )
-
-        for author_name in authors:
-            author, _created = Author.objects.get_or_create(name=author_name)
-            book.authors.add(author)
-
+        book.authors.set(authors)
         return book
+
+    @classmethod
+    def get_or_create(
+        cls,
+        *,
+        title: str,
+        authors: Sequence[Author],
+        subtitle: str = "",
+        description: str = "",
+    ) -> Tuple[Book, bool]:
+        """
+        Get or create a book with this title and authors.
+
+        Return the book along with a boolean indicating whether a new instance has been
+        created.
+
+        If the book does not exist, create it. The subtitle and description are only
+        used when creating a new Book instance.
+        """
+        books = Book.objects.filter(title=title)
+        for author in authors:
+            books = books.filter(authors=author)
+
+        if not books.exists():
+            return (
+                cls.new(
+                    title=title,
+                    subtitle=subtitle,
+                    authors=authors,
+                    description=description,
+                ),
+                True,
+            )
+        # This will throw an error if there is more than one book at this point, but
+        # that would be an integrity problem anyway.
+        return books.get(), False
