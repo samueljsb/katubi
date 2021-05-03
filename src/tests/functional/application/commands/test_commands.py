918180ded94f5e53e0ad268806422ec89c875e85
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-05-03 01:34:36 +0100

    Add tests for handling commands to record reading events

---
+++ src/tests/functional/application/commands/test_commands.py
@@ -0,0 +1,45 @@
+import datetime
+
+import pytest
+
+from katubi.application import commands, message_bus
+from katubi.reading_events import models as reading_event_models
+from tests import factories
+
+
+@pytest.mark.xfail(reason="Not implemented")
+def test_record_reading_started():
+    user = factories.User()
+    book = factories.Book()
+
+    # Queue the command to record the reading and handle it.
+    command = commands.RecordReadingStarted(
+        user=user, book=book, started_date=datetime.date(2021, 5, 2)
+    )
+    message_bus.handle([command])
+
+    # Check a single reading started event has been recorded.
+    event = reading_event_models.ReadingEvent.objects.get()
+    assert event.user == user
+    assert event.book == book
+    assert event.occurred_date == datetime.date(2021, 5, 2)
+    assert event.event_type == reading_event_models.EventType.STARTED
+
+
+@pytest.mark.xfail(reason="Not implemented")
+def test_record_reading_finished():
+    user = factories.User()
+    book = factories.Book()
+
+    # Queue the command to record the reading and handle it.
+    command = commands.RecordReadingFinished(
+        user=user, book=book, finished_date=datetime.date(2021, 5, 2)
+    )
+    message_bus.handle([command])
+
+    # Check a single reading finished event has been recorded.
+    event = reading_event_models.ReadingEvent.objects.get()
+    assert event.user == user
+    assert event.book == book
+    assert event.occurred_date == datetime.date(2021, 5, 2)
+    assert event.event_type == reading_event_models.EventType.FINISHED
