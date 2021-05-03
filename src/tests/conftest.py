6c9b39da05c7b7a99d631f392c8850a3ec727e2a
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-05-03 01:25:50 +0100

    Automatically add django_db mark to integration and functional tests
    This makes sure these tests are allowed to use the database if they need
    to.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,6 +6,20 @@ import pytest
 FIXTURES_DIR = Path(__file__).parent.absolute() / "fixtures"
 
 
+def pytest_collection_modifyitems(config, items):
+    # Loop over each test and add the appropriate marks, inferred from the location of
+    # the test module.
+    for item in items:
+        filepath = str(item.fspath)
+
+        # Ensure all integration and functional tests are run with DB marker
+        if "tests/integration" in filepath or "tests/functional" in filepath:
+            # It's possible the marker may already be there if the test requires
+            # database transaction support.
+            if "django_db" not in [m.name for m in item.own_markers]:
+                item.add_marker(pytest.mark.django_db)
+
+
 @pytest.fixture
 def json_fixture():
     """
