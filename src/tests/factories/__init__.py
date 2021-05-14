e014d273125542f8e9fe15f17efd864bf435dd58
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-05-14 20:11:53 +0100

    Add ReadingEvent factory to imports
    Prior to this change, we did not import the base ReadingEvent factory
    into the factories root.
    
    This change adds it.

--- src/tests/factories/__init__.py
+++ src/tests/factories/__init__.py
@@ -1,3 +1,7 @@
 from .auth import User  # noqa: F401
 from .books import Author, Book  # noqa: F401
-from .reading_events import FinishedReadingEvent, StartedReadingEvent  # noqa: F401
+from .reading_events import (  # noqa: F401
+    FinishedReadingEvent,
+    ReadingEvent,
+    StartedReadingEvent,
+)
