c2cbd66d837c662fc4399b2f1df73fd0c0c47329
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-05-14 20:59:57 +0100

    Refactor reading events views to use the base CommandView

--- src/katubi/api/views/_reading_events.py
+++ src/katubi/api/views/_reading_events.py
@@ -1,16 +1,12 @@
-import datetime
-from typing import Tuple
-
 from django.http import Http404
-from rest_framework import serializers
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import request, serializers
 
 from katubi import lookup
-from katubi.application import commands, message_bus
-from katubi.books import models as book_models
+from katubi.application import commands
+from katubi.application.types import Command
 from katubi.books import queries as book_queries
+
+from . import _base as base
 
 
 class RecordReadingEventFromISBNSerializer(serializers.Serializer):
@@ -18,68 +14,45 @@ class RecordReadingEventFromISBNSerializer(serializers.Serializer):
     date = serializers.DateField()
 
 
-class _RecordReadingEventFromISBN(APIView):
-    """
-    View to record a reading event.
-
-    The book that is being read is determined from the ISBN.
-    """
-
-    def get_data(self, request: Request) -> Tuple[book_models.Book, datetime.date]:
-        # Deserialize and validate the input data.
-        serializer = RecordReadingEventFromISBNSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        # Extract validated data.
-        isbn = serializer.validated_data["isbn"]
-        event_date = serializer.validated_data["date"]
-
-        # Get or create the book.
-        try:
-            book, __ = book_queries.get_or_create_from_isbn(isbn)
-        except lookup.NotFound:
-            raise Http404(f"No book found with ISBN {isbn}")
-
-        return book, event_date
-
-
-class RecordReadingStartedFromISBN(_RecordReadingEventFromISBN):
+class RecordReadingStartedFromISBN(base.CommandView):
     """
     View to record a reading started event.
 
     The book that is being read is determined from the isbn.
     """
 
-    def post(self, request: Request) -> Response:
-        # Deserialize and validate the input data.
-        book, event_date = self.get_data(request)
+    Serializer = RecordReadingEventFromISBNSerializer
 
-        # Record the reading event.
+    def get_commands(self, request: request.Request, data: dict) -> list[Command]:
+        # Get or create the book.
+        try:
+            book, __ = book_queries.get_or_create_from_isbn(data["isbn"])
+        except lookup.NotFound:
+            raise Http404(f"No book found with ISBN {data['isbn']}")
+
         command = commands.RecordReadingStarted(
-            user=request.user, book=book, started_date=event_date
+            user=request.user, book=book, started_date=data["date"]
         )
-        message_bus.handle([command])
-
-        return Response()
+        return [command]
 
 
-class RecordReadingFinishedFromISBN(_RecordReadingEventFromISBN):
+class RecordReadingFinishedFromISBN(base.CommandView):
     """
     View to record a reading finished event.
 
     The book that is being read is determined from the isbn.
     """
 
-    def post(self, request: Request) -> Response:
-        # Deserialize and validate the input data.
-        book, event_date = self.get_data(request)
+    Serializer = RecordReadingEventFromISBNSerializer
 
-        # Record the reading event.
+    def get_commands(self, request: request.Request, data: dict) -> list[Command]:
+        # Get or create the book.
+        try:
+            book, __ = book_queries.get_or_create_from_isbn(data["isbn"])
+        except lookup.NotFound:
+            raise Http404(f"No book found with ISBN {data['isbn']}")
+
         command = commands.RecordReadingFinished(
-            user=request.user,
-            book=book,
-            finished_date=event_date,
+            user=request.user, book=book, finished_date=data["date"]
         )
-        message_bus.handle([command])
-
-        return Response()
+        return [command]
