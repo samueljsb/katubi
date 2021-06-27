3520aeec1612e3a25bf331c07fd6e0276dfcb7d4
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-06-27 19:25:38 +0100

    Add a function to retrieve or create a book

--- src/katubi/books/queries.py
+++ src/katubi/books/queries.py
@@ -1,4 +1,6 @@
-from typing import Tuple
+from typing import Sequence, Tuple
+
+from django.db.models import Q
 
 from katubi import lookup
 
@@ -27,3 +29,54 @@ def get_or_create_author(name: str) -> Tuple[models.Author, bool]:
     Retrieve or create an Author record.
     """
     return models.Author.objects.get_or_create(name=name)
+
+
+def get_or_create_book(
+    *,
+    title: str,
+    authors: Sequence[models.Author],
+    subtitle: str = "",
+    description: str = "",
+) -> Tuple[models.Book, bool]:
+    """
+    Get or create a book with this title and authors.
+
+    Return the book along with a boolean indicating whether a new instance has been
+    created.
+
+    If the book does not exist, create it. The subtitle and description are only
+    used when creating a new Book instance.
+    """
+    books = models.Book.objects.filter(
+        title=title, *(Q(authors=author) for author in authors)
+    )
+
+    try:
+        return books.get(), False
+    except models.Book.DoesNotExist:
+        return (
+            _new_book(
+                title=title,
+                subtitle=subtitle,
+                authors=authors,
+                description=description,
+            ),
+            True,
+        )
+
+
+def _new_book(
+    *,
+    title: str,
+    subtitle: str,
+    authors: Sequence[models.Author],
+    description: str,
+) -> models.Book:
+    """
+    Record a new record of a book.
+    """
+    book = models.Book.objects.create(
+        title=title, subtitle=subtitle, description=description
+    )
+    book.authors.set(authors)
+    return book
