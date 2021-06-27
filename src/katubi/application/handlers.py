d249a3133e4823627cfc0d101502972ef57e4cb4
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2021-06-27 18:58:09 +0100

    Use the operations function to record new reading events

--- src/katubi/application/handlers.py
+++ src/katubi/application/handlers.py
@@ -1,4 +1,5 @@
 from katubi.reading_events import models as reading_event_models
+from katubi.reading_events import operations as reading_event_operations
 
 from . import commands
 from .types import MessageQueue
@@ -10,7 +11,7 @@ def record_reading_started(
     """
     Record a STARTED reading event.
     """
-    reading_event_models.ReadingEvent.new(
+    reading_event_operations.record_reading_event(
         user=command.user,
         book=command.book,
         event_type=reading_event_models.EventType.STARTED,
@@ -24,7 +25,7 @@ def record_reading_finished(
     """
     Record a FINISHED reading event.
     """
-    reading_event_models.ReadingEvent.new(
+    reading_event_operations.record_reading_event(
         user=command.user,
         book=command.book,
         event_type=reading_event_models.EventType.FINISHED,
